2353a217240e33d3a858598c4e1cff7f76a2cfb5
Gustavo Lira <gustavolirasn@gmail.com>
2019-05-20 19:32:11 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const readline = require('readline-sync')
+const robots = {
+  text: require('./robots/text.js')
+}
+
+async function start() {
+    const content = {
+      maximumSentences: 7
+    }
+
+    content.searchTerm = askAndReturnSearchTerm()
+    content.prefix = askAndReturnPrefix()
+
+    await robots.text(content)
+
+    function askAndReturnSearchTerm() {
+      return readline.question('Type a Wikipedia search term: ')
+    }
+
+    function askAndReturnPrefix() {
+      const prefixes = ['Who is', 'What is', 'The history of']
+      const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
+      const selectedPrefixText = prefixes[selectedPrefixIndex]
+
+      return selectedPrefixText
+    }
+
+    console.log(content)
+}
+
+start()
