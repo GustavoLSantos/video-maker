6bc49ab4549ecba12365358feab1ec32aecca53e
Gustavo Lira <gustavolirasn@gmail.com>
2019-05-20 19:35:07 -0300

    Add files via upload

---
+++ robots/text.js
@@ -0,0 +1,95 @@
+const algorithmia = require('algorithmia')
+const sentenceBoundaryDetection = require('sbd')
+const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
+
+const watsonApiKey = require('../credentials/watson-nlu.json').apikey
+const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js')
+
+const nlu = new NaturalLanguageUnderstandingV1({
+  iam_apikey: watsonApiKey,
+  version: '2018-04-05',
+  url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/'
+})
+
+async function robot(content){
+  await fetchContentFromWikipedia(content)
+  sanitizeContent(content)
+  breakContentIntoSentences(content)
+  limitMaximumSentences(content)
+
+  async function fetchContentFromWikipedia(content){
+    const algorithmiaAutenticated = algorithmia(algorithmiaApiKey)
+    const wikipediaAlgorithmia = algorithmiaAutenticated.algo("web/WikipediaParser/0.1.2")
+    const wikipediaResponde = await wikipediaAlgorithmia.pipe(content.searchTerm)
+    const wikipediaContent = wikipediaResponde.get()
+
+    content.sourceContentOriginal = wikipediaContent.content
+  }
+
+  function sanitizeContent(content) {
+    const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(content.sourceContentOriginal)
+    const withoutDatesInParentheses = removeDatesInParentheses(withoutBlankLinesAndMarkdown)
+
+    content.sourceContentSanitized = withoutDatesInParentheses
+
+    function removeBlankLinesAndMarkdown(text) {
+      const allLines = text.split('\n')
+
+      const withoutBlankLinesAndMarkdown = allLines.filter((line) => {
+        if (line.trim().length === 0 || line.trim().startsWith('=')) {
+          return false
+        }
+
+        return true
+      })
+
+      return withoutBlankLinesAndMarkdown.join(' ')
+    }
+  }
+
+  function removeDatesInParentheses(text) {
+    return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g,' ')
+  }
+
+  function breakContentIntoSentences(content) {
+    content.sentences = []
+
+    const sentences = sentenceBoundaryDetection.sentences(content.sourceContentSanitized)
+    sentences.forEach((sentence) => {
+      content.sentences.push({
+        text: sentence,
+        keywords: [],
+        images: []
+      })
+    })
+  }
+
+  async function fetchWatsonAndReturnKeywords(sentence) {
+    return new Promise((resolve, reject) => {
+      nlu.analyze({
+        text: sentence,
+        features: {
+          keywords: {}
+        }
+      }, (error, response) => {
+        if (error) {
+          reject(error)
+          return
+        }
+
+        const keywords = response.keywords.map((keyword) => {
+          return keyword.text
+        })
+
+        resolve(keywords)
+      })
+    })
+  }
+
+  function limitMaximumSentences(content){
+    content.sentences = content.sentences.slice(0, content.maximumSentences)
+  }
+
+}
+
+module.exports = robot
